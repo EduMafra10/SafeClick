a945902cdadbb649150b8ef672ae8a521b04757b
EduMafra10 <mafraduds370@gmail.com>
2026-09-10 00:10:19 -0300

    Configura a conexão com PostgreSQL e comando de verificação

--- safeclick/__init__.py
+++ safeclick/__init__.py
@@ -1,12 +1,18 @@
-#Parte feita por Eduardo Mafra, atualizado em 08-09-2026
+#Parte feita por Eduardo Mafra, atualizado em 09-09-2026
+import os
 
-from flask import Flask, redirect, url_for  #importa a classe usada para a criar a nossa aplicação.
+from flask import Flask, redirect, url_for
+
+from safeclick.db import verificar_banco
 from safeclick.simulacoes import simulacoes
 
 def create_app(): #funçao que inicia o sistema, monta e devolve nossa aplicaçao
     app = Flask(__name__) #cria a aplicacao para informar ao flask o modulo ao qual ela pertence, isso ajuda para localizar recursos do nosso projeto
 
+    app.config["DATABASE_URL"] = os.getenv("DATABASE_URL")
+
     app.register_blueprint(simulacoes)
+    app.cli.add_command(verificar_banco)
 
     @app.get("/") #associa o endereço incial do site para a funcao abaixo (inicio), o get é usado para solicitar a informaçao passada
     def inicio():
